fa6da92222575352c3223d0a033457c5e3c70c6e
jahid haque <haque.jahid@gmail.com>
2018-01-21 22:54:10 +0000

    server entry point

---
+++ server.js
@@ -0,0 +1,8 @@
+/*
+|----------------------------------------------
+| setting up server entry point for the app
+| @author: jahid haque <jahid.haque@yahoo.com>
+| @copyright: 19dnorth, 2018
+|----------------------------------------------
+*/
+
