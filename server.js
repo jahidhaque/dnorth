18b4f6ca6ad71c9b0a5004bdb4bb1130cdd8f2eb
jahid haque <haque.jahid@gmail.com>
2018-01-21 23:02:28 +0000

    updates for application entry point

--- server.js
+++ server.js
@@ -5,4 +5,21 @@
 | @copyright: 19dnorth, 2018
 |----------------------------------------------
 */
+require('dotenv').config({ silent: true });
 
+const Express = require('express');
+const Morgan = require('morgan');
+const Hbs = require('express-handlebars');
+const Path = require('path');
+const BodyParser = require('body-parser');
+
+const App = new Express();
+
+App.engine('hbs', Hbs({ defaultLayout: 'master' }));
+App.set('view engine', 'hbs');
+
+App.use(Express.static(Path.join(__dirname + '/public')));
+
+App.listen(process.env.AppPort, () => {
+    console.log(`App started on port ${process.env.AppPort}`);
+});
